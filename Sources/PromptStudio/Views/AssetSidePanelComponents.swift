9f65237674c9219e125eae6bff67779101702966
creatigo <creatigo@sbjcreatigoMac-mini.local>
2026-07-15 09:44:38 +0800

    修复侧栏标签换行重叠

--- Sources/PromptStudio/Views/AssetSidePanelComponents.swift
+++ Sources/PromptStudio/Views/AssetSidePanelComponents.swift
@@ -43,7 +43,6 @@ struct SidePanelChipFlow: View {
             }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
-        .fixedSize(horizontal: false, vertical: true)
     }
 }
 
